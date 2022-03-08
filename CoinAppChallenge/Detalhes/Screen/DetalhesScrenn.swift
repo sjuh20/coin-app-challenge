1996c465995e3d5b19936d56b3493e2b1026071f
Natanael Alves <natanalvespr@gmail.com>
2022-03-08 15:43:09 -0300

    Adicionando primeiro button

--- CoinAppChallenge/Detalhes/Screen/DetalhesScrenn.swift
+++ CoinAppChallenge/Detalhes/Screen/DetalhesScrenn.swift
@@ -9,5 +9,27 @@ import UIKit
 
 class DetalhesScrenn: UIView {
 
-   
+    lazy var backButton:UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Voltar", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        let imageBack = UIImage(named: "back")
+        imageBack?.withTintColor(UIColor.white)
+        button.setImage(imageBack, for: .normal)
+//        button.addTarget(self, action: #selector(self.tappedBackButton), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
