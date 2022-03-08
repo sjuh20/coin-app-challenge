79e2e1a61cbff3b99811ea1ebfe9403686b04351
Natanael Alves <natanalvespr@gmail.com>
2022-03-08 15:53:11 -0300

    Adicionado labels na viewBlack

--- CoinAppChallenge/Detalhes/Screen/DetalhesViewBlack.swift
+++ CoinAppChallenge/Detalhes/Screen/DetalhesViewBlack.swift
@@ -9,6 +9,74 @@ import UIKit
 
 class DetalhesViewBlack: UIView {
 
-   
+    lazy var volumeLabel:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 21)
+        valor.text = "volumes negociados"
+
+        return valor
+    }()
+    
+    lazy var ultimaHrLabel:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 17)
+        valor.text = "Última Hora"
+
+        return valor
+    }()
+
+    lazy var ultimoMesLabel:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 17)
+        valor.text = "Último Mês"
+
+        return valor
+    }()
+    
+    lazy var ultimoAnoLabel:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 17)
+        valor.text = "Último Ano"
+        
+        return valor
+    }()
+
+    lazy var valorHr:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 17)
+        valor.text = ("$")+" 123.456.78"
+        
+        return valor
+    }()
+    
+    lazy var valorMes:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 17)
+        valor.text = ("$")+" 123.456.78"
+        
+        return valor
+    }()
+    
+    lazy var valorAno:UILabel = {
+        let valor = UILabel()
+        valor.translatesAutoresizingMaskIntoConstraints = false
+        valor.textColor = .white
+        valor.font = UIFont.boldSystemFont(ofSize: 17)
+        valor.text = ("$")+" 123.456.78"
+        
+        return valor
+    }()
 
 }
