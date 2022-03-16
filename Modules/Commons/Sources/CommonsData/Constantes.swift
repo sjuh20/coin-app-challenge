f5af528832de6bd3956317a0f7ffd174857b9e35
Natanael Alves <natanalvespr@gmail.com>
2022-03-15 22:53:44 -0300

    Ajuste de código

---
+++ Modules/Commons/Sources/CommonsData/Constantes.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Natanael Alves Pereira on 15/03/22.
+//
+
+import Foundation
--- Modules/Commons/Sources/ViewModel/ViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ana Brito Souza on 09/03/22.
-//
-
-import Foundation
