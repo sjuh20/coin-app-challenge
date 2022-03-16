5d2cffeae565f37d99fcf5db02e174ca932c64ff
Natanael Alves <natanalvespr@gmail.com>
2022-03-15 22:59:56 -0300

    Alterando a Key

--- Modules/Coins/Sources/CoinModularCoins/API/APICaller.swift
+++ Modules/Coins/Sources/CoinModularCoins/API/APICaller.swift
@@ -6,74 +6,68 @@
 //
 
 import Foundation
+import CommonsData
 
 final class APICaller{
-  static let shared = APICaller()
-  
-  public var icons: [Icon] = []
-  
-  private var whenReadyBlock: ((Result<[Crypto], Error>) -> Void)?
-  
-  private struct Constants{
-    static let APIKey = "AEA7C6F6-4FCA-4C45-901E-BF07DD7741B7"
-    static let assetsEndPoint = "https://rest-sandbox.coinapi.io/v1/assets/"
-  }
-//https://rest-sandbox.coinapi.io/v1/assets/?apikey=D202342D-AA18-4966-A29F-21B09FC5C9AD
-//https://rest-sandbox.coinapi.io/v1/assets/icons/55/?apikey=AEA7C6F6-4FCA-4C45-901E-BF07DD7741B7
-//https://rest.coinapi.io/v1/assets/icons/55/?apikey=F66BC9E3-1683-4988-BBCD-D684ECDDD6BE
-  private init() {}
-  
-  public func getAllCryptoData(completion: @escaping (Result<[Crypto], Error>) -> Void){
+    static let shared = APICaller()
     
-    guard !icons.isEmpty else {
-      
-      whenReadyBlock = completion
-      return
+    public var icons: [Icon] = []
+    
+    private var whenReadyBlock: ((Result<[Crypto], Error>) -> Void)?
+    
+    private struct Constants{
+        static let APIKey = Constantes.API_KEY
+        static let assetsEndPoint = "https://rest.coinapi.io/v1/assets/"
     }
     
+    private init() {}
     
-    guard let url = URL(string: Constants.assetsEndPoint + "?apikey=" + Constants.APIKey) else { return }
-    
-    let task = URLSession.shared.dataTask(with: url) { data, _, error in
-      guard let data = data, error == nil else { return }
-      
-      do{
-        let cryptos = try JSONDecoder().decode([Crypto].self, from: data)
-        completion(.success(cryptos))
+    public func getAllCryptoData(completion: @escaping (Result<[Crypto], Error>) -> Void){
         
-        
-        
-      }catch{
-        completion(.failure(error))
-      }
-      
-      
-      
-      
-    }
-    task.resume()
-  }
-  
-  
-  func getAllIcons(){
-    guard let url = URL(string: "https://rest.coinapi.io/v1/assets/icons/55/?apikey=AEA7C6F6-4FCA-4C45-901E-BF07DD7741B7") else { return }
-    let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-      guard let data = data, error == nil else { return }
-      
-      do{
-        self?.icons = try JSONDecoder().decode([Icon].self, from: data)
-        if let completion = self?.whenReadyBlock{
-          self?.getAllCryptoData(completion: completion)
+        guard !icons.isEmpty else {
+            
+            whenReadyBlock = completion
+            return
         }
         
-      }catch{
-        print(error)
-      }
-
+        
+        guard let url = URL(string: Constants.assetsEndPoint + "?apikey=" + Constants.APIKey) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do{
+                let cryptos = try JSONDecoder().decode([Crypto].self, from: data)
+                completion(.success(cryptos))
+                
+            }catch{
+                completion(.failure(error))
+            }
+            
+        }
+        task.resume()
     }
-    task.resume()
-  }
-  
+    
+    
+    func getAllIcons(){
+        guard let url = URL(string: "https://rest.coinapi.io/v1/assets/icons/55/?apikey=\(Constants.APIKey)") else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do{
+                self?.icons = try JSONDecoder().decode([Icon].self, from: data)
+                if let completion = self?.whenReadyBlock{
+                    self?.getAllCryptoData(completion: completion)
+                }
+                
+            }catch{
+                print(error)
+            }
+            
+        }
+        task.resume()
+    }
+    
 }
 
 
--- Modules/Coins/Sources/CoinModularCoins/API/Models.swift
+++ Modules/Coins/Sources/CoinModularCoins/API/Models.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct Crypto: Codable{
-  let asset_id: String
-  let name: String?
-  let price_usd: Float?
-  let id_icon: String?
+    let asset_id: String
+    let name: String?
+    let price_usd: Float?
+    let id_icon: String?
 }
 
 struct Icon: Codable{
-  let asset_id: String
-  let url: String
+    let asset_id: String
+    let url: String
 }
